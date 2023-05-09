65c544bf60f2b6bc3e7b03245430f4f45dab6dad
Mateusz Żupa <yetiasg@gmail.com>
2023-05-09 11:35:11 +0200

    feat: add cli commands descriptions

--- src/commands/fix-all.ts
+++ src/commands/fix-all.ts
@@ -2,6 +2,8 @@ import { Command } from "@oclif/core";
 import { exec } from "child_process";
 
 export default class FixAll extends Command {
+  static description = "Run auto fix for all changed files";
+
   public async run(): Promise<void> {
     const fixAllChanges = `stylelint **/*.{scss,css} --fix`;
 
